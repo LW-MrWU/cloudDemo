8d338c4a043af376235dfef3c8eb0aba29ec1fd5
WU KUN <kwu@best-inc.com>
2021-03-05 10:12:59 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/vip/gameclub/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package vip.gameclub.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ *
+ * @author LW-MrWU
+ * @date 创建时间 2021/3/3 13:12
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
